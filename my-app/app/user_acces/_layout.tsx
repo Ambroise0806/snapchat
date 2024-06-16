9b052d838fcb3d3454dab4c0e81b37d77e0d88d0
Marco Marques <marco.marques@epitech.eu>
2024-06-16 21:58:16 +0200

    icon friends update2.0

--- my-app/app/user_acces/_layout.tsx
+++ my-app/app/user_acces/_layout.tsx
@@ -46,7 +46,7 @@ export default function TabLayout() {
           ),
         }}
       />
-            <Tabs.Screen
+      <Tabs.Screen
         name="friends"
         options={{
           tabBarIcon: ({ color, focused }) => (
